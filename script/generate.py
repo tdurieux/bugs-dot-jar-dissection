5c7ca27541e5f38bad66de389aad57ec56e37ba9
tdurieux <durieuxthomas@hotmail.com>
2018-02-19 11:45:30 +0100

    add patched bug by Elixir

--- script/generate.py
+++ script/generate.py
@@ -3,6 +3,11 @@ import json
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "data")
 
+patches = json.load(open(os.path.join(DATA_PATH, "patches.json")))
+
+for project_name in patches:
+  for (commit_id) in patches[project_name].keys():
+    patches[project_name][commit_id[0:8]] = patches[project_name][commit_id]
 bugs = []
 for project_name in os.listdir(DATA_PATH):
   project_path = os.path.join(DATA_PATH, project_name)
@@ -16,6 +21,9 @@ for project_name in os.listdir(DATA_PATH):
       bug['linesAdd'] = bug['patch'].count("\n+") - bug['files']
       bug['linesRem'] = bug['patch'].count("\n-") - bug['files']
       bug['singleLine'] = (bug['linesAdd'] == 1 and bug['linesRem'] == 0) or (bug['linesAdd'] == 0 and bug['linesRem'] == 1)
+      if project_name in patches and bug["commit"] in patches[project_name]:
+        for repair_tool in patches[project_name][bug["commit"]]:
+          bug['rt' + repair_tool] = True
       bugs += [bug]
 
 print "Processed %d bugs" % len(bugs)
